79598f0ab1bb28fe568f8988fbaa7877b9ce5c7b
Marco André Mendes <marcoandre@gmail.com>
2022-07-01 09:26:58 -0300

    criação das models Autor e Livro

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from django.db import models
 
 class Categoria(models.Model):
@@ -13,4 +14,16 @@ class Editora(models.Model):
     def __str__(self):
         return self.nome
 
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
 
+    def __str__(self):
+        return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey()
