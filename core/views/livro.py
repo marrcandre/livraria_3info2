a4d8365a6464f27aad3952e43f87cfe2d15ba0a2
Marco André Mendes <marcoandre@gmail.com>
2024-08-12 11:26:34 -0300

    If com in na ViewSet de Livro

--- core/views/livro.py
+++ core/views/livro.py
@@ -9,6 +9,6 @@ class LivroViewSet(ModelViewSet):
     serializer_class = LivroSerializer
 
     def get_serializer_class(self):
-        if self.action == "list" or self.action == "retrieve":
+        if self.action in ["list", "retrieve"]:
             return LivroDetailSerializer
         return LivroSerializer
