a35ad7b04f66e4f1080376575099c4c67d447140
Marco André Mendes <marcoandre@gmail.com>
2024-08-16 15:07:59 -0300

    Upload e Associação de Imagens.

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -10,6 +12,8 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, UserViewSet, LivroViewSet
+
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 router.register(r"autores", AutorViewSet, basename="autores")
@@ -35,6 +39,11 @@ urlpatterns = [
     # Simple JWT
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Uploader
+    path("api/media/", include(uploader_router.urls)),
     # API
     path("api/", include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
+
