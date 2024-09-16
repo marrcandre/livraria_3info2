9e5b02158552954ce173ef77e277bbdb521aa7ea
Marco André Mendes <marcoandre@gmail.com>
2024-09-16 11:47:11 -0300

    Refatorar serializers de compra para incluir criação e edição de itens de compra.

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,6 +1,6 @@
 from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
-from .compra import CompraSerializer
+from .compra import CompraSerializer, CriarEditarCompraSerializer, ItensCompraSerializer, CriarEditarItensCompraSerializer
 from .editora import EditoraSerializer
 from .livro import LivroDetailSerializer, LivroListSerializer, LivroSerializer
 from .user import UserSerializer
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -14,6 +14,11 @@ class ItensCompraSerializer(ModelSerializer):
         fields = ("quantidade", "total", "livro")
         depth = 1
 
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
@@ -23,3 +28,28 @@ class CompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = ("id", "usuario", "status", "total", "itens")
+
+
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True)
+
+    class Meta:
+        model = Compra
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
+
+    def update(self, compra, validated_data):
+        itens = validated_data.pop("itens")
+        if itens:
+            compra.itens.all().delete()
+            for item in itens:
+                ItensCompra.objects.create(compra=compra, **item)
+        compra.save()
+        return super().update(compra, validated_data)
